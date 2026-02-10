40133c01c2ded9cd4115a8320d1c75f9dcd7c744
Juan Bareno <barenojuan619@gmail.com>
2026-02-10 09:46:11 -0500

    Some modifications in Father/Son classes

---
+++ src/main/java/MODELO/Producto.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MODELO;
+
+ 
+public abstract class Producto {
+    protected int id;
+    protected double precio;
+    protected int stock;
+    protected Marca marca; // Relación: El "marca" de la tabla celular
+
+    public Producto() {}
+
+    public Producto(int id, double precio, int stock, Marca marca) {
+        this.id = id;
+        this.precio = precio;
+        this.stock = stock;
+        this.marca = marca;
+    }
+
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public double getPrecio() { return precio; }
+    public void setPrecio(double precio) { this.precio = precio; }
+
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
+
+    public Marca getMarca() { return marca; }
+    public void setMarca(Marca marca) { this.marca = marca; }
+}
